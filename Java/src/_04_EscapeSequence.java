0994b5b10c52157c870c1a82e577052126a028ab
0eunoia <118081366+0eunoia@users.noreply.github.com>
2023-06-08 11:04:37 +0900

    Special Character

---
+++ Java/src/_04_EscapeSequence.java
@@ -0,0 +1,32 @@
+public class _04_EscapeSequence {
+    public static void main(String[] args) {
+        // 특수문자, 이스케이프 문자 (Escape Sequence, Escape Character, Special Character)
+        System.out.println("자바가");
+        System.out.println("너무");
+        System.out.println("재밌어요");
+
+        // \n : 줄바꿈
+        System.out.println("자바가\n너무\n재밌어요");
+
+        // \t : 탭
+        System.out.println("해물파전\t");
+        System.out.println("김치전\t");
+        System.out.println("부추전\t");
+
+        // \\ : 역슬래시
+        System.out.println("C:\\Program Files\\Java");
+
+        // \" : 큰따옴표
+        // 고먐미가 "냐옹" 했어요
+        System.out.println("고먐미가 \"냐옹\" 이라고 했어요");
+
+        // \' : 작은따옴표
+        // 고먐미가 '뭘 봐?' 라는 표정을 지었어요.
+        System.out.println("고먐미가 \'뭘 봐?\' 라는 표정을 지었어요");
+
+        char c = 'A';
+        c = '\'';
+        System.out.println(c);
+
+    }
+}
